f68634f6f05e6eaf529fcf7650b787d11bbf57ac
ciril.tr <ciroman@gmail.com>
2021-10-16 00:38:49 +0200

    we don't need netlify, move everything to app

--- App.svelte
+++ App.svelte
@@ -1,5 +1,6 @@
 <script>
-  // import QRCode from "qrcode";
+  import QRCode from "qrcode";
+  import { encode } from "upnqr";
 
   let {
     znesek,
@@ -48,41 +49,48 @@
 
   // $: dateFormatted = rok_placila.toISOString().substr(0, 10);
 
-  // const encoder = new TextEncoder();
-  // const finalResult = encoder.encode(result);
   let qrCode;
-  // QRCode.toDataURL(
-  //   [
-  //     {
-  //       data: finalResult,
-  //       mode: "byte",
-  //       version: 15,
-  //       errorCorrectionLevel: "M"
-  //     }
-  //   ],
-  //   function(err, url) {
-  //     if (err) {
-  //       console.log(err);
-  //     } else {
-  //       qrCode = url;
-  //       // console.log(url);
-  //     }
-  //   }
-  // );
   // const handleSubmit = () => {};
   const handleSubmit = () => {
-    fetch("netlify/functions/qr-encode")
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        // console.error(error);
-        console.log("error: ", error);
-      });
+    const result = encode({
+      slog: "UPNQR",
+      polog: false,
+      dvig: false,
+      ime_placnika: "",
+      ulica_placnika: "",
+      kraj_placnika: "",
+      znesek,
+      nujno: true,
+      koda_namena,
+      namen_placila,
+      rok_placila: dateRefValue,
+      IBAN_prejemnika,
+      referenca_prejemnika,
+      ime_prejemnika,
+      ulica_prejemnika,
+      kraj_prejemnika,
+      rezerva: "dodatek do skupaj 411 znakov"
+    });
+    const encoder = new TextEncoder();
+    const finalResult = encoder.encode(result);
+    QRCode.toDataURL(
+      [
+        {
+          data: finalResult,
+          mode: "byte",
+          version: 15,
+          errorCorrectionLevel: "M"
+        }
+      ],
+      function(err, url) {
+        if (err) {
+          console.log(err);
+        } else {
+          qrCode = url;
+          // console.log(url);
+        }
+      }
+    );
   };
 </script>
 
@@ -92,74 +100,79 @@
 <main>
   <div class="container">
     <div class="columns mt-6">
-      <form on:submit|preventDefault={handleSubmit}>
-        <div class="field">
-          <label for="ime-priimek" class="label is-small">Ime in Priimek</label>
-          <div class="control">
-            <input bind:value={ime_prejemnika} name="ime-priimek" type="text" class="input"/>
+      <div class="column">
+        <form on:submit|preventDefault={handleSubmit}>
+          <div class="field">
+            <label for="ime-priimek" class="label is-small">Ime in Priimek</label>
+            <div class="control">
+              <input bind:value={ime_prejemnika} name="ime-priimek" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="ulica" class="label is-small">Ulica</label>
-          <div class="control">
-            <input bind:value={ulica_prejemnika} name="ulica" type="text" class="input"/>
+          <div class="field">
+            <label for="ulica" class="label is-small">Ulica</label>
+            <div class="control">
+              <input bind:value={ulica_prejemnika} name="ulica" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="kraj" class="label is-small">Kraj</label>
-          <div class="control">
-            <input bind:value={kraj_prejemnika} name="kraj" type="text" class="input"/>
+          <div class="field">
+            <label for="kraj" class="label is-small">Kraj</label>
+            <div class="control">
+              <input bind:value={kraj_prejemnika} name="kraj" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="iban" class="label is-small">IBAN</label>
-          <div class="control">
-            <input bind:value={IBAN_prejemnika} name="iban" type="text" class="input"/>
+          <div class="field">
+            <label for="iban" class="label is-small">IBAN</label>
+            <div class="control">
+              <input bind:value={IBAN_prejemnika} name="iban" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="referenca-prejemnika" class="label is-small">Referenca</label>
-          <div class="control">
-            <input bind:value={referenca_prejemnika} name="referenca-prejemnika" type="text" class="input"/>
+          <div class="field">
+            <label for="referenca-prejemnika" class="label is-small">Referenca</label>
+            <div class="control">
+              <input bind:value={referenca_prejemnika} name="referenca-prejemnika" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="koda-namena" class="label is-small">Koda namena plačila</label>
-          <div class="control">
-            <input bind:value={koda_namena} name="koda-namena" type="text" class="input"/>
+          <div class="field">
+            <label for="koda-namena" class="label is-small">Koda namena plačila</label>
+            <div class="control">
+              <input bind:value={koda_namena} name="koda-namena" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="namen" class="label is-small">Namen plačila</label>
-          <div class="control">
-            <input bind:value={namen_placila} name="namen" type="text" class="input"/>
+          <div class="field">
+            <label for="namen" class="label is-small">Namen plačila</label>
+            <div class="control">
+              <input bind:value={namen_placila} name="namen" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="rok-placila" class="label is-small">Rok plačila</label>
-          <div class="control">
-            <input bind:value={internal} name="rok-placila" type="date" class="input" bind:this={ dateRef }>
+          <div class="field">
+            <label for="rok-placila" class="label is-small">Rok plačila</label>
+            <div class="control">
+              <input bind:value={internal} name="rok-placila" type="date" class="input" bind:this={ dateRef }>
+            </div>
           </div>
-        </div>
 
-        <div class="field">
-          <label for="znesek" class="label is-small">Znesek v €</label>
-          <div class="control">
-            <input bind:value={znesek} name="znesek" type="text" class="input"/>
+          <div class="field">
+            <label for="znesek" class="label is-small">Znesek v €</label>
+            <div class="control">
+              <input bind:value={znesek} name="znesek" type="text" class="input"/>
+            </div>
           </div>
-        </div>
 
-          <div class="control">
-            <button type="submit" class="button is-primary is-medium">Zgeneriraj QR</button>
-          </div>
-      </form>
+            <div class="control">
+              <button type="submit" class="button is-primary is-medium">Zgeneriraj QR</button>
+            </div>
+        </form>
+      </div>
+      <div class="column">
+        <img src={qrCode} alt="">
+      </div>
     </div>
   </div>
 </main>
--- rollup.config.js
+++ rollup.config.js
@@ -9,47 +9,36 @@ import nodePolyfills from "rollup-plugin-polyfill-node";
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default [
-  {
-    input: "index.js",
-    output: {
-      sourcemap: true,
-      format: "iife",
-      name: "app",
-      file: "public/bundle.js"
-    },
-    plugins: [
-      nodePolyfills(/* options */),
-      svelte({
-        // enable run-time checks when not in production
-        dev: !production,
-        // we'll extract any component CSS out into
-        // a separate file — better for performance
-        css: (css) => {
-          css.write("public/bundle.css");
-        }
-      }),
-
-      // If you have external dependencies installed from
-      // npm, you'll most likely need these plugins. In
-      // some cases you'll need additional configuration —
-      // consult the documentation for details:
-      // https://github.com/rollup/rollup-plugin-commonjs
-      resolve(),
-      commonjs(),
-
-      // If we're building for production (npm run build
-      // instead of npm run dev), minify
-      production && terser()
-    ]
+export default {
+  input: "index.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/bundle.js"
   },
-  {
-    input: "netlify/functions/qr-encode.js",
-    output: {
-      sourcemap: true,
-      format: "cjs",
-      name: "functions",
-      dir: "functions"
-    }
-  }
-];
+  plugins: [
+    nodePolyfills(/* options */),
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file — better for performance
+      css: (css) => {
+        css.write("public/bundle.css");
+      }
+    }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs(),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ]
+};
