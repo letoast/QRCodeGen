5f5c2179df82c2d3f8982992305b41058836c036
letoast <ciroman@gmail.com>
2021-10-25 21:08:48 +0200

    Add static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import path from 'path'
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,12 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter(), // currently the adapter does not take any options
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 		vite: {
 			resolve: {
 				alias: {
