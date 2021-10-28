68d2d77d28dbab782452a8e09b3806cf91098f79
letoast <ciroman@gmail.com>
2021-10-28 10:45:11 +0000

    Fix iban field

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -89,7 +89,7 @@
 				.required()
 		}),
 		onSubmit: (values) => {
-			// alert(JSON.stringify(values));
+			alert(JSON.stringify(values));
 			// console.log(new Date(values.date));
 			generateQR(values);
 		}
@@ -313,18 +313,28 @@
 					<div>
 						<label for="iban" class="form-label">🏦 TRR</label>
 						<InputMask
-							on:blur={handleChange}
-							on:keyup={handleChange}
+							on:blur={() => validateField('IBAN_prejemnika')}
+							on:keyup={() => validateField('IBAN_prejemnika')}
 							on:customFocus={({ detail: { maskRef } }) => maskRef.updateOptions({ lazy: false })}
 							on:customBlur={({ detail: { maskRef } }) => maskRef.updateOptions({ lazy: true })}
-							on:paste={(e) =>
-								($form.IBAN_prejemnika = e.clipboardData.getData('text').replace('SI56', ''))}
+							on:paste={(e) => {
+								console.log(
+									e.clipboardData.getData('text').replace('SI56', '').replace('si56', '')
+								);
+								$form.IBAN_prejemnika = e.clipboardData
+									.getData('text')
+									.replace('SI56', '')
+									.replace('si56', '');
+							}}
 							bind:value={$form.IBAN_prejemnika}
 							name="IBAN_prejemnika"
 							inputmode="numeric"
-							unmask="typed"
+							unmask="masked"
 							imask={{
-								mask: 'SI56 0000 0000 0000 000'
+								mask: '{SI56} 0000 0000 0000 000',
+								commit: (value, mask) => {
+									return value.replace(' ', '');
+								}
 							}}
 							placeholder="SI56 1920 0123 4567 892"
 							class="form-control"
@@ -442,6 +452,8 @@
 								min: 1
 							}}
 							name="znesek"
+							inputmode="numeric"
+							pattern="[0-9]*"
 							placeholder="10"
 							type="number"
 							class="form-control"
